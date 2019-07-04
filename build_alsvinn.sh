fa24cf505a24200ac401c9adcf701a6c7d96f320
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-07-04 16:32:26 +0200

    added output

--- build_alsvinn.sh
+++ build_alsvinn.sh
@@ -2,7 +2,5 @@
 set -e
 mkdir -p alsvinn_build_folder
 cd alsvinn_build_folder
-cmake ../alsvinn -DALSVINN_USE_CUDA=OFF -DCMAKE_BUILD_TYPE=Release
+cmake ../alsvinn -DALSVINN_USE_CUDA=OFF -DCMAKE_BUILD_TYPE=Release -DALSVINN_PYTHON_VERSION=3.6 -DALSVINN_BUILD_TESTS=OFF -DALSVINN_BUILD_DOXYGEN=OFF
 make -j
-echo "### RUNNING UNIT TESTS"
-./test/library_tests/alstest
